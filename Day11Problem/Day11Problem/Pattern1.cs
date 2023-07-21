9da8701431580fdfa9f611e4efae39c305edf4a8
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-21 17:33:07 +0530

    [Prasanna] Add: Implemented the pattern Program

---
+++ Day11Problem/Day11Problem/Pattern1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11Problem
+{
+    internal class Pattern1
+    {
+        public void Pattern(int number)
+        {
+            int i, j;
+            for(i=number; i>0;i--)
+            {
+                for(j=i-1; j>0;j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+            }
+          
+        }
+    }
+}
--- Day11Problem/Day11Problem/Program.cs
+++ Day11Problem/Day11Problem/Program.cs
@@ -6,7 +6,20 @@ namespace Day11Problem
     {
         static void Main()
         {
-            Console.WriteLine("Welcome To Dayy 11 Problem Statement");
+            Console.WriteLine("Welcome To Day 11 Problem Statement");
+            bool flag = true;
+            while (flag)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        Pattern1 Pat = new Pattern1();
+                        int Number = Convert.ToInt32(Console.ReadLine());
+                        Pat.Pattern(Number);
+                        break;
+                }
+            }
         }
     }
 }
